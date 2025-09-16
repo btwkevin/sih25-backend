c4ac9218b0230648955f21340746645ce1c96414
kevin <snowdenxedward@gmail.com>
2025-09-16 16:42:55 +0530

    feat: add fiber framework

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 )
 
-func JsonObject(key string, value string) []byte {
-	mapobj := map[string]string{
-		key: value,
-	}
-	res, _ := json.Marshal(mapobj)
-	return res
-}
-
 // Todo:= add handler dir
-func HandleHealth(w http.ResponseWriter, r *http.Request) {
-	res := JsonObject("status", "healthy")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
-}
-
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	res := JsonObject("message", "404 Page Not Found")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
-}
-
-// Todo:= add fiber
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health", HandleHealth)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/health" {
-			NotFoundHandler(w, r)
-			return
-		}
+	app := fiber.New()
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "healthy",
+		})
 	})
-	fmt.Println("Server Listen : 8080")
-	http.ListenAndServe(":8080", mux)
 }
